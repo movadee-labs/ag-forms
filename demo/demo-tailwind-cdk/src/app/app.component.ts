c2f9875265a1726442263529d98d67a56ac80f1b
movadee <alla@movadee.com>
2022-10-02 18:04:42 -0400

    poc

--- demo/demo-tailwind-cdk/src/app/app.component.ts
+++ demo/demo-tailwind-cdk/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'agf-demo-tailwind-cdk',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'demo-tailwindcss-forms';
